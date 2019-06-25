aa1f806932db9b745a8c8dd63892938129e7c30c
LiZheGuang <1030660726@qq.com>
2019-06-25 12:29:03 +0800

    优化绑定按钮UI

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
   note: '增加到店自提支持',
   subDomain: "e9219a34f803fcba2d20d2bb6b44fae4", // 如果你的域名是： https://api.it120.cc/abcd 那么这里只要填写 abcd
   appid: "wx0761989662f4c2b0", // 您的小程序的appid，购物单功能需要使用
-  requireBindMobile: true // 是否强制绑定手机号码才能使用
+  requireBindMobile: false // 是否强制绑定手机号码才能使用
 }
 /*
 根据自己需要修改下单时候的模板消息内容设置，可增加关闭订单、收货时候模板消息提醒；
@@ -11,3 +11,4 @@ module.exports = {
 2、/pages/order-details/index.js 中已添加用户确认收货后提供用户参与评价；评价后提醒消费者好评奖励积分已到账；
 3、请自行修改上面几处的模板消息ID，参数为您自己的变量设置即可。  
 */
+
